5436b07381683c4a1e8c60dd828656ee6ba09a19
Rikuo Uchiki <dbkn_rikuo@keio.jp>
2020-01-10 17:18:37 +0900

    Get volume from microphone

--- Assets/Scripts/ControlFire.cs
+++ Assets/Scripts/ControlFire.cs
@@ -13,14 +13,20 @@ struct PSModules
         Em = em;
     }
 }
+
 public class ControlFire : MonoBehaviour
 {
     
-
     PSModules[] Fire = new PSModules[4];
     ParticleSystem particle;
     Vector2 PrePos;
     Vector2 NowPos;
+
+    private readonly int SampleNum = (2 << 9); // サンプリング数は2のN乗(N=5-12)
+    [SerializeField, Range(0f, 1000f)]
+    float m_gain = 200f; // 倍率
+    AudioSource m_source;
+    float[] currentValues;
 
     // Start is called before the first frame update
     void Start()
@@ -46,6 +52,21 @@ public class ControlFire : MonoBehaviour
         Fire[3].Folm = ParticleObj.forceOverLifetime;
         Fire[3].Em = ParticleObj.emission;
 
+
+        m_source = GetComponent<AudioSource>();
+        currentValues = new float[SampleNum];
+        if ((m_source != null) && (Microphone.devices.Length > 0)) // Audio source and mic exist
+        {
+            string devName = Microphone.devices[0]; // use 0th mic
+            int minFreq, maxFreq;
+            Microphone.GetDeviceCaps(devName, out minFreq, out maxFreq); // get max and min sampling num
+            int ms = minFreq / SampleNum; // take sampling time
+            m_source.loop = true; // loop
+            m_source.clip = Microphone.Start(devName, true, ms, minFreq); // set clip to mic
+            while (!(Microphone.GetPosition(devName) > 0)) { } // wait
+            Microphone.GetPosition(null);
+            m_source.Play();
+        }
     }
 
     private float mCount = 0;       //←時間計測用
@@ -81,7 +102,7 @@ public class ControlFire : MonoBehaviour
                 Fire[i].Folm.x = new ParticleSystem.MinMaxCurve(force);
             }
         }
-        
+
         /*
         mCount = mCount + Time.deltaTime;   //←時間計測中
         if (mCount >= 5.0f)
@@ -117,5 +138,15 @@ public class ControlFire : MonoBehaviour
             mSwitch = !mSwitch;
         }
         */
+
+        m_source.GetSpectrumData(currentValues, 0, FFTWindow.Hamming);
+        float sum = 0f;
+        for (int i = 0; i < currentValues.Length; ++i)
+        {
+            sum += currentValues[i]; // add data (power of each wavelength)
+        }
+        // calculate volume
+        float volumeRate = Mathf.Clamp01(sum * m_gain / (float)currentValues.Length);
+        Debug.Log(volumeRate);
     }
 }
